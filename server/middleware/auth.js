b77bbe4bac0ab6886fae65e86b3355f8809055d6
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2021-08-06 13:04:01 +0700

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
 
     if (!token)
 		return res
-			.status(401)
+			.status(200)
 			.json({ success: false, message: 'Access token not found' })
             try {
                 const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
